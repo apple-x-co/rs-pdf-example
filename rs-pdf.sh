10ba9bb64a467669e1fd3e04643871f0aecbaec8
Kouhei.Sano <apple.x.co@gmail.com>
2025-07-17 08:34:40 +0900

    Add basic image example and update README

--- rs-pdf.sh
+++ rs-pdf.sh
@@ -5,8 +5,8 @@ set -u
 
 cd "$(dirname "$0")" || exit
 
-./rs-pdf --input hello/hello.json --output hello/hello.pdf --debug --allow-override
+#./rs-pdf --input hello/hello.json --output hello/hello.pdf --debug --allow-override
 
 # basic
-./rs-pdf --input basic_text/text.json --output basic_text/text.pdf --debug --allow-override
+#./rs-pdf --input basic_text/text.json --output basic_text/text.pdf --debug --allow-override
 ./rs-pdf --input basic_image/image.json --output basic_image/image.pdf --debug --allow-override
